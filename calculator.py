280cc40259300a1f31b0ad66af2f14a02466cad8
Adhila10 <adhilaaboobakar10@gmail.com>
2024-06-18 11:18:03 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: division by zero"
+    else:
+        return a / b
+
+
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+print(f"{num1} + {num2} = {add(num1, num2)}")
+print(f"{num1} - {num2} = {subtract(num1, num2)}")
+print(f"{num1} * {num2} = {multiply(num1, num2)}")
+print(f"{num1} / {num2} = {divide(num1, num2)}")
